92f1a7f6e1189b674fd7c09e695ccc90f8584d52
developer-suhag <developersuhag@gmail.com>
2021-09-09 13:39:07 +0600

    check email

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test('example@gmail.com')
+
+
+const emailValidation = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+
+const emailField = document.getElementById('email-field');
+const validationMessage = document.getElementById('validation-message');
+
+const showMessage = (message, cls) => {
+    validationMessage.classList.add(cls)
+    validationMessage.innerText = `${message}`;
+}
+
+
+document.getElementById('check-btn').addEventListener('click', () => {
+    const email = emailField.value;
+    if (email) {
+        checkEmail(email);
+    } else {
+        showMessage('Please type your email address', 'text-warning')
+    }
+    emailField.value = '';
+});
+
+const checkEmail = email => {
+    const result = emailValidation.test(`${email}`);
+
+    // show message 
+    if (result) {
+        showMessage('✔️ Your Email Address is valid', 'text-success')
+    } else {
+        showMessage('❌ Opps your Email Address is not valid', 'text-danger')
+    }
+}
