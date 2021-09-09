e42027d5a3fc77bc7540a4d07e7ef8abecbbf5b6
developer-suhag <developersuhag@gmail.com>
2021-09-09 16:06:57 +0600

    done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test('example@gmail.com')
-
-
 const emailValidation = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 
 const emailField = document.getElementById('email-field');
 const validationMessage = document.getElementById('validation-message');
 
-const showMessage = (message, cls) => {
-    validationMessage.classList.add(cls)
+
+const showMessage = (message, color) => {
+    validationMessage.style.color = `${color}`
     validationMessage.innerText = `${message}`;
 }
 
@@ -28,8 +26,8 @@ const checkEmail = email => {
 
     // show message 
     if (result) {
-        showMessage('✔️ Your Email Address is valid', 'text-success')
+        showMessage(`✔️ ${email} :- the email address is valid`, '	#32CD32')
     } else {
-        showMessage('❌ Opps your Email Address is not valid', 'text-danger')
+        showMessage('❌ Oops, the email address is not valid', '#eb3b5a')
     }
 }
